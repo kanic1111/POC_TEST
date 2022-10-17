f8fab9db816a84aa49e7641c3db98a54af133186
admin <>
2022-10-17 08:41:47 +0000

    {/src/main/java/com/test/poc_test/POC2_1.java}

--- src/main/java/com/test/poc_test/POC2_1.java
+++ src/main/java/com/test/poc_test/POC2_1.java
@@ -28,39 +28,39 @@ public class POC2_1 implements java.io.Serializable {
 	public double getap_price_percent() {
 	   // System.out.println(AP_price_percent);
 	   //ap_price_percent = 0.9;
-	   System.out.println("Get price avg:"+ap_price_percent);
+	   //System.out.println("Get price avg:"+ap_price_percent);
 		return this.ap_price_percent;
 	}
 
 	public void setap_price_percent(java.lang.Double ap_price_percent) {
 	   // ap_price_percent = 0.9
-	    System.out.println("Set price avg:"+ap_price_percent);
+	   // System.out.println("Set price avg:"+ap_price_percent);
 		this.ap_price_percent = ap_price_percent;
 	}
 	
 	public int getap_price() {
 	   // System.out.println(customerId);
 	   //ap_price = 4000;
-	   System.out.println("Get price:"+ap_price);
+	   //System.out.println("Get price:"+ap_price);
 		return this.ap_price;
 	}
 
 	public void setap_price(java.lang.Integer ap_price) {
 	   // System.out.println(Ap_price);
 	   // ap_price = 4000;
-	   System.out.println("Set price:"+ap_price);
+	   //System.out.println("Set price:"+ap_price);
 		this.ap_price = ap_price;
 	}
 	
 	public int getmp_max_price() {
 	   // ap_price = 4000;
 	   // mp_max_price = 500;
-	    System.out.println("Get mp_price:"+mp_max_price);
+	   // System.out.println("Get mp_price:"+mp_max_price);
 		return this.mp_max_price;
 	}
 
 	public void setmp_max_price(java.lang.Integer mp_max_price) {
-	    System.out.println("Set mp_price:"+mp_max_price);
+	   // System.out.println("Set mp_price:"+mp_max_price);
 	   // System.out.println(mp_max_price);
 	   // mp_max_price = 500
 		this.mp_max_price = mp_max_price ;
@@ -68,19 +68,19 @@ public class POC2_1 implements java.io.Serializable {
 	
 	public int getlevel() {
 	   // level = 4;
-	    System.out.println("Get level:"+level);
+	   // System.out.println("Get level:"+level);
 		return this.level;
 	}
 
 	public void setlevel(java.lang.Integer level) {
 	   // level = 4
-	    System.out.println("Set level:"+level);
+	   // System.out.println("Set level:"+level);
 	   // System.out.println(level);
 		this.level = level;
 	}
 	public boolean getStatus() {
 	   // level = 4;
-	    System.out.println("Get Status:"+status);
+	   // System.out.println("Get Status:"+status);
 		return this.status;
 	}
 	
@@ -89,7 +89,7 @@ public class POC2_1 implements java.io.Serializable {
 	}
 
 	public void setStatus(boolean status) {
-	    System.out.println(status);
+	   // System.out.println(status);
 		this.status = status;
 	}
 	
