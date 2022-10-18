5739253fd5071ec930a13217ee0d539ded7bd3ec
admin <>
2022-10-18 09:10:05 +0000

    {/src/main/java/com/test/poc_test/POC2_1.java}

--- src/main/java/com/test/poc_test/POC2_1.java
+++ src/main/java/com/test/poc_test/POC2_1.java
@@ -69,7 +69,7 @@ public class POC2_1 implements java.io.Serializable {
 	public String getlevel() {
 	   // level = 4;
 	    System.out.println("Get level:"+level);
-		return "三級";
+		return this.level;
 	}
 
 	public void setlevel(java.lang.String level) {
